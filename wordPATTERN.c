09caa636ab29195d7f9a48ad5a801d69df52b4bb
DakshGopani36 <86640269+dakshgopani@users.noreply.github.com>
2023-03-12 16:06:33 +0530

    Add files via upload

---
+++ wordPATTERN.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+void main()
+{
+    int i;
+    for(int i=1;i<=30;i++) {
+        if(i==1 ||i==2 || i==9||i==15||i==18||i==21||i==22||i==23||i==24||i==27||i==30) {
+            printf("*");
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("\n");
+ for(int i=1;i<=30;i++) {
+        if(i==1||i==3 || i==8||i==10||i==15||i==17||i==21||i==27||i==30) {
+            printf("*");
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    for(int i=1;i<=30;i++) {
+        if(i==1||i==3  ||i==7||i==8||i==9|| i==10||i==11||i==15||i==21||i==22||i==23||i==24||i==27||i==28||i==29||i==30) {
+            printf("*");
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    for(int i=1;i<=30;i++) {
+        if(i==1||i==3 || i==6|| i==12||i==15||i==17||i==24||i==27||i==30) {
+            printf("*");
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("\n");
+        for(int i=1;i<=30;i++) {
+        if(i==1|| i==2||i==5||i==13||i==15||i==18||i==24||i==23||i==22||i==21||i==27||i==30) {
+            printf("*");
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
